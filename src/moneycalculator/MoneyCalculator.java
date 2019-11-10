ebcf1e1983f1527db9c9b324e8fdb8bf495f948e
LionBlue98 <LionBlue98@LAPTOP-1EO3F80H>
2019-11-10 14:09:39 +0000

    MoneyCaculator: Version2

--- src/moneycalculator/MoneyCalculator.java
+++ src/moneycalculator/MoneyCalculator.java
@@ -14,15 +14,34 @@ import java.util.Scanner;
 public class MoneyCalculator {
 
     public static void main(String[] args) throws Exception {
-        System.out.println("Introduzca una cantidad en dólares: ");
-        Scanner scanner = new Scanner(System.in);
-        double amount = Double.parseDouble(scanner.next());
-        double exchangeRate = getExchangeRate("USD","EUR");
-        System.out.println(amount + " USD equivale a " +
-                           amount*exchangeRate + " EUR");
+        MoneyCalculator moneyCalculator = new MoneyCalculator();
+        moneyCalculator.execute();
     }
     
-    private static double getExchangeRate(String from, String to) throws IOException{
+    private double amount;
+    private double exchangeRate;
+
+    private void execute() throws IOException {
+        input();
+        process();
+        output();
+    }
+
+    private void input() {
+        System.out.println("Introduzca una cantidad de dólares");
+        Scanner scanner = new Scanner(System.in);
+        amount = Double.parseDouble(scanner.next());
+    }
+
+    private void process() throws IOException {
+        exchangeRate = getExchangeRate("USD", "EUR");
+    }
+
+    private void output() {
+        System.out.println(amount + " USD equivale a " + amount * exchangeRate + " EUR");
+    }
+    
+    /*private static double getExchangeRate(String from, String to) throws IOException{
         URL url = new URL("https://api.exchangeratesapi.io/latest?base=" + from);
         URLConnection connection = url.openConnection();
         try{
@@ -31,13 +50,27 @@ public class MoneyCalculator {
             String line = reader.readLine();
             JsonParser parser = new JsonParser();
             JsonObject gsonObj = parser.parse(line).getAsJsonObject();
-            String linel = line.substring(line.indexOf(to)+12,line.indexOf("}"));
+            //String linel = line.substring(line.indexOf(to)+12,line.indexOf("}"));
             JsonPrimitive moneyPrimitive = gsonObj.getAsJsonPrimitive(to);
-            int money = moneyPrimitive.getAsInt();
-            return Double.parseDouble(linel);
+            double money = moneyPrimitive.getAsDouble();
+            return money;
         }catch(IOException exception){
             System.out.println("IOException encontrada: " + exception);
             return -1;
+        }
+    }*/
+    
+    private static double getExchangeRate(String from, String to) throws IOException {
+        URL url = 
+            new URL("http://free.currencyconverterapi.com/api/v5/convert?q=" +
+                    from + "_" + to + "&compact=y");
+        URLConnection connection = url.openConnection();
+        try (BufferedReader reader = 
+                new BufferedReader(
+                        new InputStreamReader(connection.getInputStream()))) {
+            String line = reader.readLine();
+            String line1 = line.substring(line.indexOf(to)+12, line.indexOf("}"));
+            return Double.parseDouble(line1);
         }
     }
 }
