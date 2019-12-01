6d28f8860a98a17022ea12c3eadbfaabe95b4e4a
LionBlue98 <LionBlue98@LAPTOP-1EO3F80H>
2019-12-01 13:21:37 +0000

    MoneyCalculator: Finalized

---
+++ src/moneycalculator/control/MainFrame.java
@@ -0,0 +1,81 @@
+package moneycalculator.control;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.HashMap;
+import java.util.Map;
+import javax.swing.*;
+import moneycalculator.view.Command;
+import moneycalculator.view.MoneyDialog;
+import moneycalculator.view.MoneyDisplay;
+
+public class MainFrame extends JFrame{
+    
+    private JTextField amount;
+    private final Map<String,Command> commands = new HashMap<>();
+    private final MoneyDisplay display;
+    private final MoneyDialog dialog;
+    
+    public MainFrame(MoneyDisplay display, MoneyDialog dialog){
+        setTitle("MoneyCalculator");
+        setResizable(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+        this.display = display;
+        this.dialog = dialog;
+        
+        getContentPane().add(toolBar());
+        
+        pack();
+        setLocationRelativeTo(null);
+        setVisible(true);
+        setSize(new Dimension(300,200));
+        setMinimumSize(new Dimension(300,200));
+    }
+    
+    private JPanel toolBar(){
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        amount = new JTextField(20);
+        JPanel data = new JPanel();
+        data.setLayout(new FlowLayout());
+        data.add(amount);
+        
+        JPanel buttons = new JPanel();
+        buttons.setLayout(new FlowLayout());
+        
+        buttons.add(createButton("Calculate"));
+        buttons.add(createButton("Cancel"));
+        
+        panel.add(data);
+        panel.add(dialog.getPanel());
+        panel.add(display.getPanel());
+        panel.add(buttons);
+        
+        return panel;
+    }
+    
+    private JButton createButton(String name){
+        JButton button = new JButton(name);
+        button.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) throws NumberFormatException{
+                commands.get(name).toExecute();
+            }
+        });
+        return button;
+    }
+    
+    public JTextField getAmount(){
+        return amount;
+    }
+    
+    public void setAmount(String s){
+        amount.setText(s);
+    }
+    
+    public void addCommand(String id, Command command){
+        commands.put(id, command);
+    }
+}
